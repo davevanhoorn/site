fe7fa28814b578c80173ca7fcc8b5bb7b7a38678
Dave van Hoorn <yo@davevanhoorn.com>
2019-11-02 16:10:33 +0100

    Temporarily remove newsletter component - not finished

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,8 +3,8 @@ import React from "react"
 import Newsletter from "../newsletter/Newsletter"
 
 const Footer = () => (
-  <footer className="pt-10 pb-6 text-xs text-gray-700 bg-gray-200">
-    <Newsletter />
+  <footer className="pt-10 pb-6 text-xs text-gray-700">
+    {/* <Newsletter /> */}
     <div className="text-center">
       <span className="ml-1 block" role="img" aria-label="Heart">
         ❤️

83eeeeae6df0ca654d702cab0e62ab53477729b7
Dave van Hoorn <yo@davevanhoorn.com>
2019-11-02 16:12:53 +0100

    Remove Newsletter component

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,7 @@
 import React from "react"
 
-import Newsletter from "../newsletter/Newsletter"
-
 const Footer = () => (
   <footer className="pt-10 pb-6 text-xs text-gray-700">
-    {/* <Newsletter /> */}
     <div className="text-center">
       <span className="ml-1 block" role="img" aria-label="Heart">
         ❤️
